a1b531aab51b0cd4f1c8cbe0245e7dc8bafc48f3
Jason M. Dudley <j.m.duds@gmail.com>
2015-04-08 20:53:56 -0500

    Finished CH 1.  JavaScript refresher.

--- 1-angularjs-get-started-exercise-files/before/script.js
+++ 1-angularjs-get-started-exercise-files/before/script.js
@@ -1,2 +1,28 @@
 // Code goes here
 
+var createWorker = function() {
+	var hiddenCount = 0;
+
+	var task1 = function() {
+		hiddenCount++;
+		console.log("task1 " + hiddenCount);
+	};
+	var task2 = function() {
+		hiddenCount++;
+		console.log("task2");
+	};
+	return {
+		job1: task1,
+		job2: task2
+	};
+};
+
+var worker = createWorker();
+
+worker.job1();
+worker.job2();
+worker.job1();
+worker.job1();
+worker.job1();
+worker.job2();
+worker.job1();
