9fb0504dcf4bbb4c4b62df96a75a981583f25ced
Jason M. Dudley <j.m.duds@gmail.com>
2015-04-09 21:52:40 -0500

    Removed controller global variable.
    Finished chapter 2.  Better controller design.

--- 2-angularjs-get-started-exercise-files/before/script.js
+++ 2-angularjs-get-started-exercise-files/before/script.js
@@ -1,16 +1,22 @@
 // Code goes here
+(function() {
 
-var MainController = function($scope, $http) {
-	var onUserComplete = function(response) {
-		$scope.user = response.data;
+	var app = angular.module("githubViewer", []);
+
+	var MainController = function($scope, $http) {
+		var onUserComplete = function(response) {
+			$scope.user = response.data;
+		};
+
+		var onError = function(reason) {
+			$scope.error = "Could not fetch the user";
+		};
+
+		$http.get("https://api.github.com/users/robconery")
+			.then(onUserComplete, onError);
+
+		$scope.message = "TEST!";
 	};
 
-	var onError = function(reason) {
-		$scope.error = "Could not fetch the user";
-	};
-
-	$http.get("https://api.github.com/users/robconery")
-		.then(onUserComplete, onError);
-
-	$scope.message = "TEST!";
-};
+	app.controller("MainController", ["$scope", "$http", MainController]);
+}());
